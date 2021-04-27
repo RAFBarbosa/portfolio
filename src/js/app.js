970451b545afd27b9b522b7f02d2c9a29a902657
Raf Barbosa <rcbar89@gmail.com>
2021-04-26 21:32:44 -0400

    animations

--- src/js/app.js
+++ src/js/app.js
@@ -22,39 +22,52 @@ window.addEventListener("scroll", () => {
   document.querySelector(".arrow__wrapper").style.opacity = opacity;
 });
 
+const $openHeader = document.querySelector(".header__contact");
+const $openFooter = document.querySelector(".footer__contact");
+const $close = document.querySelector(".contact__close");
+const $pageBlur = document.querySelectorAll(".blur__bg");
+const $body = document.querySelector("body");
+const $contact = document.querySelector(".contact");
+const $contactTitle = document.querySelector(".contact__title");
+const $contactText = document.querySelector(".contact__text");
+
 (function () {
-  const $openHeader = document.querySelector(".header__contact");
-  const $openFooter = document.querySelector(".footer__contact");
-  const $close = document.querySelector(".contact__close");
-  const $pageBlur = document.querySelectorAll(".blur__bg");
-
   $openHeader.addEventListener("click", () => {
-    $openHeader.style.display = "none";
-    document.querySelector(".contact").style.display = "flex";
-    document.querySelector("body").style.overflow = "hidden";
-
-    for (let i = 0; i < $pageBlur.length; i++) {
-      $pageBlur[i].style.filter = "blur(5px)";
-    }
+    setTimeout(openContact, 300);
   });
 
   $openFooter.addEventListener("click", () => {
-    $openHeader.style.display = "none";
-    document.querySelector(".contact").style.display = "flex";
-    document.querySelector("body").style.overflow = "hidden";
-    
-    for (let i = 0; i < $pageBlur.length; i++) {
-      $pageBlur[i].style.filter = "blur(5px)";
-    }
+    setTimeout(openContact, 300);
   });
 
   $close.addEventListener("click", () => {
-    $openHeader.style.display = "flex";
-    document.querySelector(".contact").style.display = "none";
-    document.querySelector("body").style.overflow = "auto";
-    
-    for (let i = 0; i < $pageBlur.length; i++) {
-      $pageBlur[i].style.filter = "blur(0px)";
-    }
+    closeContact();
   });
 })();
+
+function openContact() {
+  $openHeader.style.display = "none";
+  // $contact.style.display = "flex";
+  $body.style.overflow = "hidden";
+  $contact.classList.toggle("active");
+  setTimeout(function () {
+    $contactTitle.classList.toggle("active");
+  }, 600);
+  setTimeout(function () {
+    $contactText.classList.toggle("active");
+  }, 800);
+
+  for (let i = 0; i < $pageBlur.length; i++) {
+    $pageBlur[i].style.filter = "blur(5px)";
+  }
+}
+
+function closeContact() {
+  // $openHeader.style.display = "flex";
+  $contact.style.display = "none";
+  $body.style.overflow = "auto";
+
+  for (let i = 0; i < $pageBlur.length; i++) {
+    $pageBlur[i].style.filter = "blur(0px)";
+  }
+}
