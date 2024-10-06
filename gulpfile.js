c1f2b53827b48cd5a98aaf26834cfb1b223a287c
RAF Barbosa <rcbar89@gmail.com>
2024-10-05 17:19:21 -0700

    updated to 2024

--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,71 @@
-const { watch, series, src, dest } = require('gulp');
-const sass = require('gulp-sass');
-const connect = require('gulp-connect');
+const { watch, series, src, dest } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const connect = require("gulp-connect");
 
 // js
-const babelify = require('babelify');
-const source = require('vinyl-source-stream');
-const browserify = require('browserify');
-const buffer = require('vinyl-buffer');
- 
-sass.compiler = require('node-sass');
+const babelify = require("babelify");
+const source = require("vinyl-source-stream");
+const browserify = require("browserify");
+const buffer = require("vinyl-buffer");
+
+sass.compiler = require("sass");
 
 function server() {
-  return connect.server({
-    root: 'build',
-    livereload: true
-  });
+	return connect.server({
+		root: "build",
+		livereload: true,
+	});
 }
 
 function javascript(cb) {
-  const bundler = browserify({
-    entries: [
-      './node_modules/babel-polyfill/dist/polyfill.js',
-      './src/js/app.js',
-    ],
-  }).transform(
-    babelify.configure({
-      presets: ['@babel/preset-env'],
-    })
-  );
+	const bundler = browserify({
+		entries: [
+			"./node_modules/babel-polyfill/dist/polyfill.js",
+			"./src/js/app.js",
+		],
+	}).transform(
+		babelify.configure({
+			presets: ["@babel/preset-env"],
+		})
+	);
 
-  return bundler
-    .bundle()
-    .pipe(source('app.js'))
-    .pipe(buffer())
-    .pipe(dest('./build'))
-    .pipe(connect.reload());
+	return bundler
+		.bundle()
+		.pipe(source("app.js"))
+		.pipe(buffer())
+		.pipe(dest("./build"))
+		.pipe(connect.reload());
 }
 
 function css(cb) {
-  return src('./src/sass/style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(dest('./build'))
-    .pipe(connect.reload());
+	return src("./src/sass/style.scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(dest("./build"))
+		.pipe(connect.reload());
 }
 
 function html(cb) {
-  return src('./src/index.html')
-    .pipe(dest('./build'))
-    .pipe(connect.reload());
+	return src("./src/index.html").pipe(dest("./build")).pipe(connect.reload());
 }
 
 function public(cb) {
-  return src('./public/*')
-    .pipe(dest('./build/public/'))
-    .pipe(connect.reload());
+	return src("./public/*")
+		.pipe(dest("./build/public/"))
+		.pipe(connect.reload());
 }
 
 function img(cb) {
-  return src('./public/img/*')
-    .pipe(dest('./build/public/img'))
-    .pipe(connect.reload());
+	return src("./public/img/*")
+		.pipe(dest("./build/public/img"))
+		.pipe(connect.reload());
 }
 
 exports.build = series(html, css, javascript, public, img);
 
 exports.watch = function () {
-  server();
+	server();
 
-  watch('./src/sass/**/*.scss', css);
-  watch('./src/js/**/*.js', javascript);
-  watch('./src/**/*.html', html);
-}
+	watch("./src/sass/**/*.scss", css);
+	watch("./src/js/**/*.js", javascript);
+	watch("./src/**/*.html", html);
+};
--- src/js/projects.js
+++ src/js/projects.js
@@ -1,87 +1,90 @@
 export default function Request() {
-  const $wrapper = document.querySelector(".projects__wrapper");
+	const $wrapper = document.querySelector(".projects__wrapper");
 
-  fetch("../public/projects.json")
-    .then(function (data) {
-      return data.json();
-    })
-    .then((data) => {
-      const project = data;
+	fetch("../public/projects.json")
+		.then(function (data) {
+			return data.json();
+		})
+		.then((data) => {
+			const project = data;
 
-      for (let i = 0; i < project.length; i++) {
-        const $aosWrapper = document.createElement("div");
-        const $projectsWrapperItem = document.createElement("div");
-        const $textContentWrapper = document.createElement("article");
-        const $photo = document.createElement("img");
-        const $photoAlt = document.createAttribute("alt");
-        const $name = document.createElement("h2");
-        const $description = document.createElement("p");
-        const $toolsTitle = document.createElement("h3");
-        const $toolsUl = document.createElement("ul");
-        const $link = document.createElement("a");
-        const $divider = document.createElement("hr");
+			for (let i = 0; i < project.length; i++) {
+				const $aosWrapper = document.createElement("div");
+				const $projectsWrapperItem = document.createElement("div");
+				const $textContentWrapper = document.createElement("article");
+				const $photo = document.createElement("img");
+				const $photoAlt = document.createAttribute("alt");
+				const $name = document.createElement("h2");
+				const $description = document.createElement("p");
+				const $toolsTitle = document.createElement("h3");
+				const $toolsUl = document.createElement("ul");
+				const $link = document.createElement("a");
+				const $divider = document.createElement("hr");
 
-        $projectsWrapperItem.classList.add("projects__wrapper__item");
-        $textContentWrapper.classList.add("projects__wrapper__item__text");
+				$projectsWrapperItem.classList.add("projects__wrapper__item");
+				$textContentWrapper.classList.add(
+					"projects__wrapper__item__text"
+				);
 
-        // ---- AOS FADE IN TRANSITION ---- //
-        const $dataAos = document.createAttribute("data-aos");
-        const $dataDuration = document.createAttribute("data-aos-duration");
-        const $dataDelay = document.createAttribute("data-aos-delay");
-        const $dataOnce = document.createAttribute("data-aos-once");
+				// ---- AOS FADE IN TRANSITION ---- //
+				const $dataAos = document.createAttribute("data-aos");
+				const $dataDuration =
+					document.createAttribute("data-aos-duration");
+				const $dataDelay = document.createAttribute("data-aos-delay");
+				const $dataOnce = document.createAttribute("data-aos-once");
 
-        $dataAos.value = "fade-up";
-        $dataDuration.value = "1000";
-        $dataDelay.value = "700";
-        $dataOnce.value = "true";
+				$dataAos.value = "fade-up";
+				$dataDuration.value = "1000";
+				$dataDelay.value = "700";
+				$dataOnce.value = "true";
 
-        $aosWrapper.setAttributeNode($dataAos);
-        $aosWrapper.setAttributeNode($dataDuration);
-        $aosWrapper.setAttributeNode($dataDelay);
-        $aosWrapper.setAttributeNode($dataOnce);
-        // -------------------------------- //
+				$aosWrapper.setAttributeNode($dataAos);
+				$aosWrapper.setAttributeNode($dataDuration);
+				$aosWrapper.setAttributeNode($dataDelay);
+				$aosWrapper.setAttributeNode($dataOnce);
+				// -------------------------------- //
 
-        $photo.src = project[i].photo;
-        $name.innerHTML = project[i].name;
-        $photoAlt.value = project[i].name + " photo";
-        $description.innerHTML = project[i].description;
-        $toolsTitle.innerHTML = "Tools used in this project:";
-        $link.innerHTML = project[i].link;
+				$photo.src = project[i].photo;
+				$name.innerHTML = project[i].name;
+				$photoAlt.value = project[i].name + " photo";
+				$description.innerHTML = project[i].description;
+				$toolsTitle.innerHTML = "Tools used in this project:";
+				$link.innerHTML = project[i].link;
 
-        const tools = project[i].tools;
+				const tools = project[i].tools;
 
-        for (let j = 0; j < tools.length; j++) {
-          const listItem = document.createElement("li");
-          listItem.textContent = "+ " + tools[j];
-          $toolsUl.appendChild(listItem);
-        }
+				for (let j = 0; j < tools.length; j++) {
+					const listItem = document.createElement("li");
+					listItem.textContent = "+ " + tools[j];
+					$toolsUl.appendChild(listItem);
+				}
 
-        $textContentWrapper.appendChild($name);
-        $textContentWrapper.appendChild($description);
-        $textContentWrapper.appendChild($toolsTitle);
-        $textContentWrapper.appendChild($toolsUl);
-        $projectsWrapperItem.appendChild($textContentWrapper);
-        $projectsWrapperItem.appendChild($photo);
-        $photo.setAttributeNode($photoAlt);
-        $aosWrapper.appendChild($projectsWrapperItem);
+				$textContentWrapper.appendChild($name);
+				$textContentWrapper.appendChild($description);
+				$textContentWrapper.appendChild($toolsTitle);
+				$textContentWrapper.appendChild($toolsUl);
+				$projectsWrapperItem.appendChild($textContentWrapper);
+				$projectsWrapperItem.appendChild($photo);
+				$photo.setAttributeNode($photoAlt);
+				$aosWrapper.appendChild($projectsWrapperItem);
 
-        if (i + 1 < project.length) {
-          $aosWrapper.appendChild($divider);
-        }
+				if (i + 1 < project.length) {
+					$aosWrapper.appendChild($divider);
+				}
 
-        $wrapper.appendChild($aosWrapper);
+				$wrapper.appendChild($aosWrapper);
 
-        // $aosWrapper.addEventListener("click", () => {
-        //   addModal(
-        //     $name.innerHTML,
-        //     $description.innerHTML,
-        //     $photo.src,
-        //     $toolsUl.innerHTML,
-        //     $link.innerHTML
-        //   );
-        // });
-      }
-    });
+				// $aosWrapper.addEventListener("click", () => {
+				//   addModal(
+				//     $name.innerHTML,
+				//     $description.innerHTML,
+				//     $photo.src,
+				//     $toolsUl.innerHTML,
+				//     $link.innerHTML
+				//   );
+				// });
+			}
+		});
 }
 
 // function addModal(name, description, photo, tools, link) {
